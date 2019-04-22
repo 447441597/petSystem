637b3adbba47e839b9a4f75fdcab90f1a249229a
Qian Li <421734841@qq.com>
2019-04-22 15:10:13 +0800

    增加

---
+++ petLoves/src/components/goods/add.vue
@@ -0,0 +1,116 @@
+<template>
+<div>
+   <el-dialog title="增加商品" :visible.sync="dialogFormVisible" width="35%">
+      <el-form :model="form">
+        <el-form-item label="商品名称：" :label-width="formLabelWidth">
+          <el-input v-model="form.name" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="商品类型：" :label-width="formLabelWidth">
+          <el-input v-model="form.type" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="材质或制作方法：" :label-width="formLabelWidth">
+          <el-input v-model="form.material" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="适用规格：" :label-width="formLabelWidth">
+          <el-input v-model="form.applyGuige" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="专属规格：" :label-width="formLabelWidth">
+          <el-input v-model="form.exGuige" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="包装规格：" :label-width="formLabelWidth">
+          <el-input v-model="form.Packing" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="口味：" :label-width="formLabelWidth">
+          <el-input v-model="form.taste" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="特殊功用：" :label-width="formLabelWidth">
+          <el-input v-model="form.specialFunc" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="产地：" :label-width="formLabelWidth">
+          <el-input v-model="form.addr" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="生产日期：" :label-width="formLabelWidth">
+          <el-input v-model="form.productionDate" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="保质期：" :label-width="formLabelWidth">
+          <el-input v-model="form.keepDate" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="供应商：" :label-width="formLabelWidth">
+          <el-input v-model="form.provider" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="特色说明：" :label-width="formLabelWidth">
+          <el-input v-model="form.features" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="价格：" :label-width="formLabelWidth">
+          <el-input v-model="form.price" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="图片：" :label-width="formLabelWidth">
+          <el-upload
+            action="https://jsonplaceholder.typicode.com/posts/"
+            list-type="picture-card"
+            :on-preview="handlePictureCardPreview"
+            :on-remove="handleRemove"
+          >
+            <i class="el-icon-plus"></i>
+          </el-upload>
+          <el-dialog :visible.sync="dialogVisible">
+            <img width="100%" :src="dialogImageUrl" alt>
+          </el-dialog>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="dialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click="dialogFormVisible = false">确 定</el-button>
+      </div>
+    </el-dialog>
+</div>
+</template>
+
+<script>
+export default {
+data() {
+    return {
+         dialogImageUrl: "",
+        dialogVisible: false,
+      dialogFormVisible: false,
+      form: {
+        name: "",
+        type: "",
+        material: "",
+        applyGuige: "",
+        exGuige: "",
+        Packing: "",
+        taste: "",
+        specialFunc: "",
+        addr: "",
+        keepDate: "",
+        productionDate: "",
+        provider: "",
+        features: "",
+        price: "",
+      },
+      formLabelWidth: "140px"
+    };
+  },
+  methods: {
+       handleClose(done) {
+      this.$confirm("确认关闭？")
+        .then(_ => {
+          done();
+        })
+        .catch(_ => {});
+    },
+    handleRemove(file, fileList) {
+      console.log(file, fileList);
+    },
+    handlePictureCardPreview(file) {
+      this.dialogImageUrl = file.url;
+      this.dialogVisible = true;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>

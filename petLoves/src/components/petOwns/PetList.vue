b7c10e99213ee69840ab73d03b9ba712242f74a8
xjf <634001650@example.com>
2019-04-22 14:50:20 +0800

    增加列表

---
+++ petLoves/src/components/petOwns/PetList.vue
@@ -0,0 +1,20 @@
+<template>
+        <tbody>
+        <tr>
+            <td>姓名</td>
+            <td>年龄</td>
+            <td>性别</td>
+            <td>
+                <button>删除</button>
+                <button>修改</button>
+            </td>
+        </tr>
+        </tbody>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
--- petLoves/src/components/petOwns/index.vue
+++ petLoves/src/components/petOwns/index.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
         <h1>宠主管理</h1>
+        <PetList></PetList>
     </div>
 </template>
 
 <script>
+import PetList from "./PetList";
 export default {
-
-}
+  components: {
+    PetList
+  }
+};
 </script>
 
 <style>
-
 </style>

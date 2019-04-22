783524eeb3692dd6af1ab0bd4836325766df1da8
lwj <766200093@qq.com>
2019-04-22 16:37:06 +0800

    lwj

--- petLoves/src/store/index.js
+++ petLoves/src/store/index.js
@@ -1,25 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-<<<<<<< HEAD
 import zyz from './zyz'
+import lwj from './lwj'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules:{
-    zyz,
-=======
-// import lwj from './lwj'
-Vue.use(Vuex)
-
-export default new Vuex.Store({
-  state: {
-
-  },
-  mutations: {
-    // lwj
-  },
-  actions: {
-
->>>>>>> lwj
+    zyz,lwj
   }
 })

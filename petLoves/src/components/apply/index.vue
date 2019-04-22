e4c1c334ef7c1ae86bd94fd7e6badb20a5ddf15b
lwj <766200093@qq.com>
2019-04-22 11:10:39 +0800

    提交门店管理

--- petLoves/src/components/apply/index.vue
+++ petLoves/src/components/apply/index.vue
@@ -1,15 +1,88 @@
 <template>
     <div>
-        <h1>申请</h1>
+       <el-button type="primary"  @click="dialogFormVisible = true">门店注册</el-button>
+       <el-dialog title="增加门店" :visible.sync="dialogFormVisible">
+         <el-form :model="form">
+           <el-form-item label="门店名称" :label-width="formLabelWidth">
+              <el-input v-model="form.name" autocomplete="off"></el-input>
+           </el-form-item>
+           <el-form-item label="营业执照号码" :label-width="formLabelWidth">
+              <el-input v-model="form.businessNum" autocomplete="off"></el-input>
+           </el-form-item>
+           <el-form-item label="营业执照" :label-width="formLabelWidth">
+             <el-upload
+                     class="upload-demo"
+                     action="https://jsonplaceholder.typicode.com/posts/"
+                     :on-preview="handlePreview"
+                     :on-remove="handleRemove"
+                     :before-remove="beforeRemove"
+                     multiple
+                     :limit="3"
+                     :on-exceed="handleExceed"
+                     :file-list="fileList">
+                <el-button size="small" type="primary">点击上传</el-button>
+              </el-upload>
+           </el-form-item>
+           <el-form-item label="法人" :label-width="formLabelWidth">
+              <el-input v-model="form.legalPerson" autocomplete="off"></el-input>
+           </el-form-item>
+            <el-form-item label="电话" :label-width="formLabelWidth">
+              <el-input v-model="form.tel" autocomplete="off"></el-input>
+           </el-form-item>
+            <el-form-item label="头图" :label-width="formLabelWidth">
+              <el-upload
+                  class="avatar-uploader"
+                  action="https://jsonplaceholder.typicode.com/posts/"
+                  :show-file-list="false"
+                  :on-success="handleAvatarSuccess"
+                  :before-upload="beforeAvatarUpload">
+                  <img v-if="imageUrl" :src="imageUrl" class="avatar">
+                  <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+              </el-upload>
+           </el-form-item>
+            <el-form-item label="特色" :label-width="formLabelWidth">
+              <el-input v-model="form.feature" autocomplete="off"></el-input>
+           </el-form-item>
+            <el-form-item label="VIP等级" :label-width="formLabelWidth">
+              <el-input v-model="form.vipLeval" autocomplete="off"></el-input>
+           </el-form-item>
+           <el-form-item label="佣金比例" :label-width="formLabelWidth">
+              <el-input v-model="form.rate" autocomplete="off"></el-input>
+           </el-form-item>
+            <el-form-item label="店员姓名" :label-width="formLabelWidth">
+              <el-input v-model="form.assistantname" autocomplete="off"></el-input>
+           </el-form-item>
+            <el-form-item label="店员职级" :label-width="formLabelWidth">
+              <el-input v-model="form.assistantlevel" autocomplete="off"></el-input>
+           </el-form-item>
+            <el-form-item label="店员电话" :label-width="formLabelWidth">
+              <el-input v-model="form.assistantphone" autocomplete="off"></el-input>
+           </el-form-item>
+        </el-form>
+           <div slot="footer" class="dialog-footer">
+              <el-button @click="dialogFormVisible = false">取 消</el-button>
+              <el-button type="primary"  @click="add" >确 定</el-button>
+           </div>
+       </el-dialog>
     </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      dialogFormVisible: false,
+      form: {},
+      formLabelWidth: "120px"
+    };
+  },
+  methods:{
+    add() {
+        this.dialogFormVisible = false;
+    },
+  }
+};
 </script>
 
 <style>
-
 </style>
--- petLoves/src/components/shops/index.vue
+++ petLoves/src/components/shops/index.vue
@@ -1,15 +1,57 @@
 <template>
     <div>
-        <h1>门店管理</h1>
+       <el-button type="primary"  @click="dialogFormVisible = true">门店注册</el-button>
+       <el-dialog title="增加门店" :visible.sync="dialogFormVisible">
+         <el-form :model="form">
+           <el-form-item label="姓名" :label-width="formLabelWidth">
+              <el-input v-model="form.name" autocomplete="off"></el-input>
+           </el-form-item>
+           <el-form-item label="营业执照号码" :label-width="formLabelWidth">
+              <el-input v-model="form.businessNum" autocomplete="off"></el-input>
+           </el-form-item>
+           <el-form-item label="法人" :label-width="formLabelWidth">
+              <el-input v-model="form.legalPerson" autocomplete="off"></el-input>
+           </el-form-item>
+            <el-form-item label="电话" :label-width="formLabelWidth">
+              <el-input v-model="form.tel" autocomplete="off"></el-input>
+           </el-form-item>
+            <el-form-item label="特色" :label-width="formLabelWidth">
+              <el-input v-model="form.feature" autocomplete="off"></el-input>
+           </el-form-item>
+            <el-form-item label="VIP等级" :label-width="formLabelWidth">
+              <el-input v-model="form.vipLeval" autocomplete="off"></el-input>
+           </el-form-item>
+           <el-form-item label="佣金比例" :label-width="formLabelWidth">
+              <el-input v-model="form.rate" autocomplete="off"></el-input>
+           </el-form-item>
+            <el-form-item label="佣金比例" :label-width="formLabelWidth">
+              <el-input v-model="form.rate" autocomplete="off"></el-input>
+           </el-form-item>
+        </el-form>
+           <div slot="footer" class="dialog-footer">
+              <el-button @click="dialogFormVisible = false">取 消</el-button>
+              <el-button type="primary"   >确 定</el-button>
+           </div>
+       </el-dialog>
     </div>
 </template>
 
 <script>
 export default {
-
-}
+  data() {
+    return {
+      dialogFormVisible: false,
+      form: {
+        gender: "",
+        name: "",
+        age: "",
+        delivery: false
+      },
+      formLabelWidth: "120px"
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>

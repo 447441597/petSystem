cb1e3bb8e68b054694a201006efdc2ff1e8436c7
dongwei <15008318929@163.com>
2019-04-23 10:09:58 +0800

    解决冲突

--- petLoves/src/store/index.js
+++ petLoves/src/store/index.js
@@ -1,26 +1,23 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-// import zyz from './zyz'
-// import users from './users'
+import zyz from './zyz'
+import users from './users'
 import petowns from './petowns'
 import services from './services'
-// import lwj from './lwj'
+import lwj from './lwj'
 // import yj from './yj'
-// import goods from 'goods'
+import goods from 'goods'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
 
   modules: {
-    petowns, services
-  },
-  state: {
+    petowns, services,
+    users,services,goods,lwj,zyz
 
   },
-  actions: {
-    
-  }
+ 
 
 })
 

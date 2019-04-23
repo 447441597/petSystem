3e0be01b26a6685695be6994d999f2208a5ac9eb
dongwei <15008318929@163.com>
2019-04-23 10:14:16 +0800

    更新修改

--- petLoves/src/store/index.js
+++ petLoves/src/store/index.js
@@ -2,10 +2,10 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 
 
-import orders from './ordrers'
-// import users from './users'
+// import orders from './ordrers'
+import users from './users'
 // import petowns from './petowns'
-import services from './services'
+// import services from './services'
 // import lwj from './lwj'
 // import yj from './yj'
 // import goods from 'goods'
@@ -13,9 +13,10 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules:{
-    orders,
-    // users,petowns,
-    services,
+    // orders,
+    users,
+    // petowns,
+    // services,
     // goods,lwj,yj
   }
 })

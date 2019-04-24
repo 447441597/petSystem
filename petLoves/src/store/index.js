261771bf10dc4a4b60692c469b55f72b3f7062a6
YangFan <447441597@qq.com>
2019-04-24 15:15:31 +0800

    xiugai

--- petLoves/src/store/index.js
+++ petLoves/src/store/index.js
@@ -1,19 +1,24 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import yj from './yj'
-import goods from './goods'
-import lwj from './lwj'
-import ordrers from './ordrers'
-import petowns from './petowns'
-import services from './services'
-import users from './users'
-import serverType from './serverType'
-Vue.use(Vuex)
+import Vue from "vue";
+import Vuex from "vuex";
+import yj from "./yj";
+import goods from "./goods";
+import lwj from "./lwj";
+import ordrers from "./ordrers";
+import petowns from "./petowns";
+import services from "./services";
+import users from "./users";
+import serverType from "./serverType";
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    petowns, services,ordrers,users,lwj,goods,yj,serverType
+    petowns,
+    services,
+    ordrers,
+    users,
+    lwj,
+    goods,
+    yj,
+    serverType
   }
-
 });
-

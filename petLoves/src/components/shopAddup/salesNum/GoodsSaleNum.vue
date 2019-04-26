74de06758a040c0c24b12ea21fef1c51d5d5da67
lwj <766200093@qq.com>
2019-04-26 08:58:59 +0800

    提交统计

--- petLoves/src/components/shopAddup/salesNum/GoodsSaleNum.vue
+++ petLoves/src/components/shopAddup/salesNum/GoodsSaleNum.vue
@@ -24,14 +24,22 @@ import "echarts/extension/bmap/bmap";
 import axios from "axios";
 import { createNamespacedHelpers } from "vuex";
 const { mapState, mapActions } = createNamespacedHelpers("lwj");
-
+let arr = [];
 export default {
   data() {
     return {
       type: "商品销售统计图",
       shopsData: [],
       shopid: "",
-      userId: ""
+      userId: "",
+      spring: 0,
+      summer: 0,
+      aut: 0,
+      winter: 0,
+      shopData: [],
+      yearData: [],
+      year: [],
+      yearArr: []
     };
   },
   mounted() {
@@ -48,15 +56,123 @@ export default {
         method: "get",
         url: "/getSession"
       }).then(res => {
-        this.userId = res.data._id;
+        console.log(res.data, "1312312313315112121");
+        this.shopid = res.data.shopsId;
       });
+      // console.log(this.shopid, "sadfbnsadfnbsadgnmsadfghm");
       let myChart = echarts.init(this.$refs.myChart);
       if (this.type == "月销售统计") {
         axios({
-          url: "/applys/shopaTotal/" + this.userId,
+          url: "/orders/all",
+          method: "get"
+        }).then(res => {
+          // console.log(res, "res");
+          for (let j = 1; j <= 12; j++) {
+            let n = "0";
+            if (j >= 10) {
+              n = "";
+            }
+            let str = 2019 + "-" + n + j;
+            let num = 0;
+            for (let i = 0; i < res.data.length; i++) {
+              let time = res.data[i].time.substring(0, 7);
+              if (res.data[i].status == "订单已完成") {
+                if (res.data[i].shops._id == this.shopid && time == str) {
+                  num++;
+                }
+              }
+            }
+            this.shopsData.push(num);
+          }
+          // console.log(this.shopsData, "11111111111111111111");
+          myChart.setOption(this.classesOptions, true);
+        });
+      } else if (this.type == "季度销售统计") {
+        let k = 0;
+        this.spring = 0;
+        this.summer = 0;
+        this.aut = 0;
+        this.winter = 0;
+        axios({
+          url: "/orders/all",
           method: "get"
         }).then(res => {
           console.log(res, "res");
+          for (let j = 1; j <= 12; j++) {
+            let n = "0";
+            if (j >= 10) {
+              n = "";
+            }
+            let str = 2019 + "-" + n + j;
+            let num = 0;
+            for (let i = 0; i < res.data.length; i++) {
+              console.log(res.data.length, "12313231");
+              let time = res.data[i].time.substring(0, 7);
+              if (res.data[i].status == "订单已完成") {
+                if (res.data[i].shops._id == this.shopid && time == str) {
+                  num++;
+                }
+              }
+            }
+            this.shopData.push(num);
+          }
+          console.log(this.shopData, "11111111111111111111");
+          for (k; k < this.shopData.length; k++) {
+            if (k >= 2 && k <= 4) {
+              this.spring += this.shopData[k];
+            } else if (k >= 5 && k <= 7) {
+              this.summer += this.shopData[k];
+            } else if (k >= 8 && k <= 10) {
+              this.aut += this.shopData[k];
+            } else if (k == 11 || k == 0 || k == 1) {
+              this.winter += this.shopData[k];
+            }
+          }
+          console.log(this.spring, this.summer, this.aut, this.winter);
+          myChart.setOption(this.jiduOptions, true);
+        });
+      } else if (this.type == "年销售统计") {
+        axios({
+          url: "/orders/all",
+          method: "get"
+        }).then(res => {
+          console.log(res, "res");
+           console.log(res.data.length,"--------------------------------")
+           let time = ""
+          for (let j = 1; j <= res.data.length; j++) {
+           
+            // let n = "0";
+            // if (j >= 10) {
+            //   n = "";
+            // }
+             time += res.data[j].time.substring(0, 4)
+            this.year.push(time);
+            this.yearArr.push(res.data[j]);
+            console.log(this.year, "12312313123123saddsdsadasdas");
+            let num = 0;
+            console.log(this.yearArr,"scv55555555555555555555")
+            for (let i = 0; i <= this.year.length; i++) {
+              for (let k = 0; k <= this.yearArr.length; k++) {
+                if (this.yearArr[k].status == "订单已完成") {
+                  if (this.yearArr[k].shops._id == this.shopid && time == this.year[i].time) {
+                    num++;
+                  }
+                }
+              }
+              this.yearData.push(num)
+            }
+            console.log(this.yearData,"qwertwqerwqerwqer")
+            // for (let i = 0; i < res.data.length; i++) {
+            //   let time = res.data[i].time.substring(0, 7);
+            //   if (res.data[i].status == "订单已完成") {
+            //     if (res.data[i].shops._id == this.shopid && time == str) {
+            //       num++;
+            //     }
+            //   }
+            // }
+          }
+          // console.log(this.shopsData, "11111111111111111111");
+          myChart.setOption(this.yearOptions, true);
         });
       }
     }
@@ -90,7 +206,85 @@ export default {
           {
             name: "销售",
             type: "bar",
-            data: [5, 10, 20, 5, 16, 4, 8, 10, 9, 16, 14, 8]
+            data: this.shopsData
+          }
+        ]
+      };
+    },
+    jiduOptions() {
+      return {
+        title: {
+          text: "年销售统计图",
+          subtext: "",
+          x: "center"
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+          orient: "vertical",
+          left: "left"
+          // data: ["春", "夏", "秋", "冬"]
+        },
+        series: [
+          {
+            name: "",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "60%"],
+            data: [
+              { value: this.spring, name: "春" },
+              { value: this.summer, name: "夏" },
+              { value: this.aut, name: "秋" },
+              { value: this.winter, name: "冬" }
+            ],
+            itemStyle: {
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              }
+            }
+          }
+        ]
+      };
+    },
+    yearOptions() {
+      return {
+        title: {
+          text: "季度销售统计图",
+          subtext: "",
+          x: "center"
+        },
+        tooltip: {
+          trigger: "item",
+          formatter: "{a} <br/>{b} : {c} ({d}%)"
+        },
+        legend: {
+          orient: "vertical",
+          left: "left",
+          data: ["春", "夏", "秋", "冬"]
+        },
+        series: [
+          {
+            name: "",
+            type: "pie",
+            radius: "55%",
+            center: ["50%", "60%"],
+            data: [
+              { value: this.spring, name: "春" },
+              { value: this.summer, name: "夏" },
+              { value: this.aut, name: "秋" },
+              { value: this.winter, name: "冬" }
+            ],
+            itemStyle: {
+              emphasis: {
+                shadowBlur: 10,
+                shadowOffsetX: 0,
+                shadowColor: "rgba(0, 0, 0, 0.5)"
+              }
+            }
           }
         ]
       };
